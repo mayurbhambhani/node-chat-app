d14e240c6f736a6961ceb8eb02725d06fe7c74a9
mayur bhambhani <mayur@infoworks.io>
2017-05-28 09:27:03 +0530

    using http

--- server/server.js
+++ server/server.js
@@ -1,13 +1,18 @@
-//const config = require("./config/config.js")
+const config = require("./config/config.js")
 
 const path = require("path");
 const express = require("express");
+const socketIO = require("socket.io");
+const http = require("http");
+
 
 let app = express();
-
 let public_folder = path.join(__dirname, "../", "public");
 app.use(express.static(public_folder));
 
-app.listen(process.env.PORT, () => {
+let server = http.createServer(app);
+
+
+server.listen(process.env.PORT, () => {
     console.log(`listening on port ${process.env.PORT}`);
 })
